ea7391b659923ece2b7495fe00076a24095e0f97
Segun Ojo <theuncleshege@gmail.com>
2022-10-27 00:01:23 +0100

    Added DevOps stuff

---
+++ api/scripts/start.dev.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+chmod +x ./scripts/check_db.sh && ./scripts/check_db.sh
+php artisan migrate:fresh --force --seed
+
+if [[ -z ${APP_KEY} ]]; then
+    echo APP_KEY=$(php artisan key:generate --show) >> ~/.bashrc
+    source ~/.bashrc
+fi
+
+php artisan config:clear
+php artisan route:clear
+php artisan cache:clear
+php artisan view:clear
+
+# php artisan config:cache
+# php artisan route:cache
+
+service php7.2-fpm start && nginx -g "daemon off;"
